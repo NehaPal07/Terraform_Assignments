05a9c47ca73939bb5d3228cd0c24441f5110da91
EC2 Default User <ec2-user@ip-172-31-30-34.us-east-2.compute.internal>
2020-05-05 04:50:27 +0000

    Added files

---
+++ main.tf
@@ -0,0 +1,10 @@
+############################### Resource_Docker Image #########################################
+
+data "docker_registry_image" "nginx" {
+  name = "nginx:latest"
+}
+
+resource "docker_image" "nginx" {
+  name = "${data.docker_registry_image.nginx.name}"
+  pull_triggers = ["${data.docker_registry_image.nginx.name}"] 
+}
